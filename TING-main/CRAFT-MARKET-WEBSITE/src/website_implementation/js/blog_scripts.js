1ff0008eab199b9d1e6181fdc0114d53c803e98f
Tingyi Wei <140461020+weilinfamu@users.noreply.github.com>
2023-10-13 21:26:28 +1000

    tried to search some better picture to create sliding effect
    also change the colour of the buttons and change the layout to column, which means that central the main part to adjust other device to meet responsiveness

--- TING-main/CRAFT-MARKET-WEBSITE/src/website_implementation/js/blog_scripts.js
+++ TING-main/CRAFT-MARKET-WEBSITE/src/website_implementation/js/blog_scripts.js
@@ -3,62 +3,38 @@ $(document).ready(function() {
     const slides = $('.carousel .slide');
     const slideWidth = $(slides[0]).outerWidth();
     const numberOfSlides = slides.length;
-  
-    // Add clones to the beginning and end
-    $('.carousel').prepend($(slides[slides.length - 1]).clone());
-    $('.carousel').append($(slides[0]).clone());
-  
+
     // Adjust the carousel width and initial position
-    $('.carousel').css('width', slideWidth * (numberOfSlides + 2) + 'px');
-    $('.carousel').css('transform', 'translateX(' + (-slideWidth) + 'px)');
-  
+    $('.carousel').css('width', slideWidth * numberOfSlides + 'px');
+
     $('#next').click(function() {
         moveToNextSlide();
     });
-  
+
     $('#prev').click(function() {
         moveToPreviousSlide();
     });
-  
-    // 自动轮播功能
+
+    // 添加3秒轮播效果
     setInterval(moveToNextSlide, 3000);
-  
+
     function moveToNextSlide() {
         currentSlide++;
-        if (currentSlide > numberOfSlides) {
-            currentSlide = 1;
-            $('.carousel').css('transition', 'none');
-            $('.carousel').css('transform', 'translateX(' + (-slideWidth) + 'px)');
-            setTimeout(() => {
-                $('.carousel').css('transition', 'transform 0.4s ease-in-out');
-                nextSlide();
-            });
-            return;
+        if (currentSlide >= numberOfSlides) {
+            currentSlide = 0;
         }
-        nextSlide();
+        updateSlide();
     }
-  
+
     function moveToPreviousSlide() {
         currentSlide--;
         if (currentSlide < 0) {
             currentSlide = numberOfSlides - 1;
-            $('.carousel').css('transition', 'none');
-            $('.carousel').css('transform', 'translateX(' + (-slideWidth * numberOfSlides) + 'px)');
-            setTimeout(() => {
-                $('.carousel').css('transition', 'transform 0.4s ease-in-out');
-                prevSlide();
-            });
-            return;
         }
-        prevSlide();
+        updateSlide();
     }
-  
-    function nextSlide() {
-        $('.carousel').css('transform', 'translateX(' + (-slideWidth * (currentSlide + 1)) + 'px)');
+
+    function updateSlide() {
+        $('.carousel').css('transform', 'translateX(' + (-slideWidth * currentSlide) + 'px)');
     }
-  
-    function prevSlide() {
-        $('.carousel').css('transform', 'translateX(' + (-slideWidth * (currentSlide + 1)) + 'px)');
-    }
-  });
-  
+});
